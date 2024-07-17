0b6c00f77a410969e7371e127d4885bbd85a4864
satokihanada <satoki0212@gmail.com>
2024-07-17 22:07:47 +0900

    feat: Eject current project, Add baic background service

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,8 +1,45 @@
-import { StyleSheet, ImageBackground, Dimensions, View } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
 import RootNavigation from "./src/navigation/RootNavigation";
+import { NativeBaseProvider, extendTheme } from "native-base";
+import { useEffect } from "react";
+import nodejs from "nodejs-mobile-react-native";
+
+const config = {
+  useSystemColorMode: true,
+  components: {
+    Button: {
+      defaultProps: {
+        colorScheme: "info",
+      },
+    },
+    IconButton: {
+      defaultProps: {
+        colorScheme: "info",
+      },
+    },
+    Input: {
+      defaultProps: {
+        colorScheme: "info",
+      },
+    },
+  },
+};
+
+const theme = extendTheme({ ...config });
 
 export default function App() {
-  return <RootNavigation />;
+  useEffect(() => {
+    console.log(nodejs);
+    // nodejs.start("main.js");
+    // nodejs.channel.addListener("message", (msg) => {
+    //   alert("From node: " + msg);
+    // });
+  });
+  return (
+    <NativeBaseProvider theme={theme}>
+      <RootNavigation />
+    </NativeBaseProvider>
+  );
 }
 
 const styles = StyleSheet.create({
