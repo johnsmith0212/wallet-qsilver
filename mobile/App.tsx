4c183f7c090cbb01b26797ab6276dc70ad898fa5
ishida0415 <ishida0415k@gmail.com>
2024-07-18 11:26:23 +0900

    Merge branch 'app' into feature/richlist

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,50 +1,8 @@
-import { StyleSheet, Dimensions, Text } from "react-native";
+import { StyleSheet, ImageBackground, Dimensions, View } from "react-native";
 import RootNavigation from "./src/navigation/RootNavigation";
-import { Button, NativeBaseProvider, extendTheme } from "native-base";
-import { useEffect, useState } from "react";
-import { Provider } from "react-redux";
-import { store } from "./src/redux/store";
-import { channelInit, login } from "./src/api/api";
-import Toast from "react-native-toast-message";
-
-const config = {
-  useSystemColorMode: true,
-  components: {
-    Button: {
-      defaultProps: {
-        colorScheme: "info",
-      },
-    },
-    IconButton: {
-      defaultProps: {
-        colorScheme: "info",
-      },
-    },
-    Input: {
-      defaultProps: {
-        colorScheme: "info",
-      },
-    },
-  },
-};
-
-const theme = extendTheme({ ...config });
 
 export default function App() {
-  const [text, setText] = useState("");
-  useEffect(() => {
-    channelInit();
-  }, []);
-  return (
-    <Provider store={store}>
-      <NativeBaseProvider theme={theme}>
-        <RootNavigation />
-        <Toast />
-        {/* <Text>{text}</Text> */}
-        {/* <Button onPress={() => login("123")}>onPress</Button> */}
-      </NativeBaseProvider>
-    </Provider>
-  );
+  return <RootNavigation />;
 }
 
 const styles = StyleSheet.create({
