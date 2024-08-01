080dbd279091856e3f952aaf9ff7dc416fc4bec5
hitosisasaki <hitosisasaki0212@gmail.com>
2024-08-01 11:43:52 +0900

    fix: market price error fix
    Co-authored-by: johnsmith0212 <john0212.skill@gmail.com>
    Co-authored-by: hitosisasaki <hitosisasaki0212@gmail.com>

--- app/client/src/utils/interfaces.ts
+++ app/client/src/utils/interfaces.ts
@@ -1,30 +1,6 @@
-type ModeProps = {
-    wsUrl: string;
-    type: 'mainnet' | 'testnet';
-    // add some more if need
-}
+export type TransactionItem = [number, string, string, string];
 
-type SidebarItemProps = {
-    icon: string;
-    label: string;
-    link: string;
-    active?: boolean;
-    onClick?: () => void;
+export type RichList = {
+    name: string,
+    richlist: [[number, string, string]],
 };
-
-
-type SummaryItemProps = {
-    icon: string;
-    label: string;
-    amount: string;
-};
-
-type AssetItemProps = {
-    icon: string;
-    name: string;
-    amount: string;
-    percentage: number;
-    colorClassName: string;
-};
-
-export type { SidebarItemProps, SummaryItemProps, AssetItemProps, ModeProps }
