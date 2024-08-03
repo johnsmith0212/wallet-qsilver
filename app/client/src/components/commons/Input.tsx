7350696bde074229f4b937e9d977a33469096e1d
hitosisasaki <hitosisasaki0212@gmail.com>
2024-08-03 20:47:45 +0900

    feat: Add new restore page
    Co-authored-by: johnsmith0212 <john0212.skill@gmail.com>
    Co-authored-by: hitosisasaki <hitosisasaki0212@gmail.com>

--- app/client/src/components/commons/Input.tsx
+++ app/client/src/components/commons/Input.tsx
@@ -6,7 +6,6 @@ type InputProps = {
     inputStyle?: "forms" | "base" | "modal" | "custom";
     gapVariant?: "strech" | "base" | "xs";
     backgroundVariant?: "base" | "off-white" | "transparent";
-    value?: string | number;
     onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
@@ -18,8 +17,7 @@ const Input = ({
     gapVariant = "base",
     backgroundVariant = "base",
     placeholder,
-    value,
-    onChange,
+    onChange
 }: InputProps) => {
     const backgrounds = {
         base: "bg-dark-input",
@@ -68,7 +66,6 @@ const Input = ({
                 className={style.input}
                 placeholder={placeholder ? placeholder : ""}
                 min={0}
-                value={value}
                 onChange={onChange}
             />
         </div>
